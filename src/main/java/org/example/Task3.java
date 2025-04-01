a12e73ec7cd7a2156e9cf70e1fbbe0e7672abefa
Francisco Ismael Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-04-01 18:32:33 +0200

    task3 ok

---
+++ src/main/java/org/example/Task3.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 6;
+        System.out.println(Math.pow(x, 2)+Math.pow(((4*y)/5-x),2));
+
+    }
+}
